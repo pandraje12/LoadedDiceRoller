0d6deb1a336091b653959e3b2c018a0382f630c0
pandraje12 <119981327+pandraje12@users.noreply.github.com>
2022-12-16 10:53:31 -0800

    Added Model for loaded dice

---
+++ Models/LoadedDice.cs
@@ -0,0 +1,11 @@
+﻿namespace DiceRoller.Models
+{
+    public class LoadedDice
+    {
+        public int favoredFace1 { get; set; }
+        public int favoredFactor1 { get; set; }
+        public int favoredFace2 { get; set; }
+        public int favoredFactor2 { get; set; }
+        //int numRolls
+    }
+}
